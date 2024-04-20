1c1dbfa7e33e3b8c605563cbdd126b62b7b7fde3
Adarsh <adarsh9540984202@gmail.com>
2024-04-20 21:44:36 +0530

    update it

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,110 @@
+import React, { useEffect, useState } from 'react';
+// import '../..';
+import { CreateTodo } from './createTodo';
+import axios from 'axios';
+
+
+interface Todo {
+  _id: string;
+  title: string;
+  description: string;
+  completed: boolean; 
+}
+
+function Dashboard() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const fetchTodos = async () => {
+    try {
+      const response = await fetch("https://todo-backend-jade-iota.vercel.app/todos");
+      const json = await response.json();
+      setTodos(json.todos);
+    } catch (error) {
+      console.error('Error fetching todos:', error);
+    }
+  };
+
+  const deleteTodo = async (id: string) => {
+    try {
+      await axios.delete(`https://todo-backend-jade-iota.vercel.app/todo/${id}`);
+      fetchTodos();
+    } catch (error) {
+      console.error('Error deleting todo:', error);
+    }
+  };
+
+  const toggleCompletion = async (id: string) => {
+    try {
+      const updatedTodos = todos.map(todo => {
+        if (todo._id === id) {
+          const updatedTodo = { ...todo, completed: !todo.completed };
+          setTodos(prevTodos => [...prevTodos.filter(t => t._id !== id), updatedTodo]);
+          return updatedTodo;
+        }
+        return todo;
+      });
+      await axios.put(`https://todo-backend-jade-iota.vercel.app/todo/${id}`, { completed: !todos.find(todo => todo._id === id)?.completed });
+    } catch (error) {
+      console.error('Error toggling todo completion:', error);
+    }
+  };
+  
+
+const addTodo = async (newTodo: Todo) => {
+  try {
+    console.log('Adding todo:', newTodo);
+    await fetch("https://todo-backend-jade-iota.vercel.app/todo", {
+      method: "POST",
+      body: JSON.stringify(newTodo),
+      headers: {
+        "Content-type": "application/json"
+      }
+    });
+    console.log('Todo added successfully');
+    await fetchTodos(); 
+  } catch (error) {
+    console.error('Error adding todo:', error);
+  }
+};
+
+
+
+  useEffect(() => {
+    fetchTodos();
+  }, [todos]);
+
+  return (
+   <>
+   <div className='bg-red-700'>
+    <h1 className='text-white p-4 text-3xl font-serif font-bold flex items-center justify-center'>Add Task</h1>
+   </div>
+    <div className='flex flex-col md:flex md:flex-row min-h-screen bg-black justify-evenly'>
+      <div className='flex flex-col p-6 rounded-lg'>
+        <CreateTodo addTodo={addTodo} />
+      </div>
+
+      <div className="mt-4 ">
+        <h2 className='text-white font-bold text-xl m-4'>Your Todo List</h2>
+        {todos.map((todo: Todo, index: number) => (
+          <div key={todo._id} className="bg-white shadow-md rounded-lg p-4 mb-4 flex flex-col md:flex md:flex-row justify-between ">
+            <div>
+              <h2 className="text-lg font-semibold">{`${index + 1}. ${todo.title}`}</h2>
+              <p className="text-gray-600">{todo.description}</p>
+            </div>
+            <div className='mt-2 md:mt-0 flex flex-row md:justify-between items-center'>
+              {todo.completed ? (
+                <span className='text-green-500 font-bold'><img src="https://cdn1.iconfinder.com/data/icons/warnings-and-dangers/400/Warning-02-512.png" className='w-6' alt="" /></span>
+              ) : (
+                <button onClick={() => toggleCompletion(todo._id)} className='bg-yellow-400 hover:bg-yellow-600 text-white rounded-md px-4 py-2 mx-2'>Mark as completed</button>
+              )}
+              <button onClick={() => deleteTodo(todo._id)} className='bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-md ml-2'>Delete</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+   </>
+  );
+}
+
+export default Dashboard;
--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,7 +1,17 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-export function SignUp() {
-  const [formData, setFormData] = useState({
+interface SignUpFormData {
+  firstname: string;
+  lastname: string;
+  email: string;
+  password: string;
+}
+
+const SignUp: React.FC = () => {
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState<SignUpFormData>({
     firstname: "",
     lastname: "",
     email: "",
@@ -23,14 +33,13 @@ export function SignUp() {
 
       if (response.ok) {
         console.log("User signed up successfully");
-       
+        // Redirect to the dashboard page upon successful signup
+        navigate("/dashboard");
       } else {
         console.error("Failed to sign up:", await response.text());
-      
       }
     } catch (error) {
       console.error("Error signing up:", error);
-      
     }
   };
 
@@ -42,18 +51,20 @@ export function SignUp() {
   };
 
   return (
-    <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
+
+    <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black items-center  justify-center flex flex-col mt-24  bg-slate-300">
       <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
         Welcome to TodoApp
       </h2>
 
       <form className="my-8" onSubmit={handleSubmit}>
-        <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
+        <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4 ">
           <div className="flex flex-col space-y-2 w-full">
             <label htmlFor="firstname">First name</label>
             <input
               id="firstname"
               type="text"
+              className="p-1"
               placeholder="Adarsh"
               value={formData.firstname}
               onChange={handleChange}
@@ -62,6 +73,7 @@ export function SignUp() {
           <div className="flex flex-col space-y-2 w-full">
             <label htmlFor="lastname">Last name</label>
             <input
+             className="p-1"
               id="lastname"
               type="text"
               placeholder="Singh"
@@ -73,6 +85,7 @@ export function SignUp() {
         <div className="flex flex-col space-y-2 w-full mb-4">
           <label htmlFor="email">Email Address</label>
           <input
+           className="p-1"
             id="email"
             type="email"
             placeholder="projectmayhem@fc.com"
@@ -83,6 +96,7 @@ export function SignUp() {
         <div className="flex flex-col space-y-2 w-full mb-4">
           <label htmlFor="password">Password</label>
           <input
+           className="p-1"
             id="password"
             type="password"
             placeholder="••••••••"
@@ -91,7 +105,7 @@ export function SignUp() {
           />
         </div>
         <button
-          className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+          className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset] p-1"
           type="submit"
         >
           Sign up &rarr;
@@ -100,3 +114,5 @@ export function SignUp() {
     </div>
   );
 }
+
+export default SignUp;
